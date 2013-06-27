7af282c06f5840e4a1bba19e381638ffee8297eb
Jen Fong-Adwent <jen@ednapiranha.com>
2013-06-27 10:21:27 -0400

    fixed buffer upload

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
 module.exports = function (app, nconf, isLoggedIn) {
   var levelup = require('levelup');
   var leveldown = require('leveldown');
-  var request = require('request');
+  var needle = require('needle');
   var uuid = require('uuid');
   var knox = require('knox');
 
@@ -17,7 +17,7 @@ module.exports = function (app, nconf, isLoggedIn) {
 
       if (db) {
         db.createReadStream({
-          limit: 25
+          limit: 30
         }).on('data', function (data) {
           memes.push({
             url: data.value.url,
@@ -42,77 +42,86 @@ module.exports = function (app, nconf, isLoggedIn) {
   });
 
   app.post('/add', isLoggedIn, function (req, res, next) {
+    var uid = uuid.v1();
     var buffer = new Buffer(req.body.photo, 'base64');
+    var filename = 'meme-' + uid + '.' + req.body.fileType;
+    var user;
 
-    var s3 = knox.createClient({
-      key: nconf.get('s3_key'),
-      secret: nconf.get('s3_secret'),
-      bucket: nconf.get('s3_bucket')
-    });
-
-    var headers = {
-      'Content-Type': 'image/' + req.body.fileType
+    var updateDb = function (url, callback) {
+      levelup(nconf.get('db'), {
+        createIfMissing: true,
+        keyEncoding: 'binary',
+        valueEncoding: 'json'
+      }, function (err, db) {
+        if (db) {
+          db.put('meme_' + uid, {
+            url: url,
+            loginType: req.session.loginType,
+            user: user
+          }, function () {
+            db.get('meme_' + uid, function (e, p) {
+              callback();
+              db.close();
+            });
+          });
+        } else {
+          res.status(500);
+          next(err);
+        }
+      });
     };
 
-    var uid = uuid.v1();
-    var filename = 'meme-' + uid + '.' + req.body.fileType;
+    if (req.session.loginType === 'appdotnet') {
+      user = req.session.passport.user.id;
 
-    s3.putBuffer(buffer, filename, headers, function (err) {
-      if (err) {
-        res.status(500);
-        next(err);
-      } else {
-        levelup(nconf.get('db'), {
-          createIfMissing: true,
-          keyEncoding: 'binary',
-          valueEncoding: 'json'
-        }, function (err, db) {
-          if (db) {
-            var user;
+      // use app.net's file api
+      var data = {
+        content: {
+          buffer: buffer,
+          filename: filename,
+          content_type: 'image/png',
+        },
+        public: true,
+        type: 'photo'
+      };
 
-            if (req.session.loginType === 'appdotnet') {
-              user = req.session.passport.user.id;
-            } else {
-              user = req.session.email;
-            }
-
-            db.put('meme_' + uid, {
-              url: s3.url(filename),
-              loginType: req.session.loginType,
-              user: user
-            }, function () {
-              db.get('meme_' + uid, function (e, p) {
-                if (req.session.loginType === 'appdotnet') {
-                  // use app.net's file api
-                  request({
-                    method: 'POST',
-                    url: 'https://alpha-api.app.net/stream/0/files',
-                    headers: {
-                      'Authorization': 'Bearer ' + req.session.passport.user.access_token
-                    },
-                    json: {
-                      content: buffer,
-                      public: true,
-                      type: 'photo'
-                    }
-                  }, function (err, resp, body) {
-                    console.log(body)
-                    res.redirect('/meme/' + encodeURIComponent(p.url));
-                    db.close();
-                  });
-                } else {
-                  res.redirect('/meme/' + encodeURIComponent(p.url));
-                  db.close();
-                }
-              });
-            });
-          } else {
-            res.status(500);
-            next(err);
-          }
+      needle.post('https://alpha-api.app.net/stream/0/files', data, {
+        headers: {
+          'Authorization': 'Bearer ' + req.session.passport.user.access_token
+        },
+        multipart: true
+      }, function (err, resp, body) {
+        updateDb(body.data.url_permanent, function () {
+          res.redirect('/meme/' + encodeURIComponent(body.data.url_permanent));
         });
-      }
-    });
+      });
+
+    } else {
+
+      // using s3
+      var s3 = knox.createClient({
+        key: nconf.get('s3_key'),
+        secret: nconf.get('s3_secret'),
+        bucket: nconf.get('s3_bucket')
+      });
+
+      var headers = {
+        'Content-Type': 'image/' + req.body.fileType
+      };
+
+      user = req.session.email;
+
+      s3.putBuffer(buffer, filename, headers, function (err) {
+        if (err) {
+          res.status(500);
+          next(err);
+        } else {
+          updateDb(s3.url(filename), function () {
+            res.redirect('/meme/' + encodeURIComponent(s3.url(filename)));
+          });
+        }
+      });
+    }
   });
 
   app.get('/meme/:url', function (req, res, next) {
